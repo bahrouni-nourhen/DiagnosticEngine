83e7f253d00a924518a1523f7fdf918e1f13c1bd
bahrouni-norhen <norhen.bahrouni@enicar.ucar.tn>
2026-08-04 17:39:24 +0100

    Add ISO-TP adapter interface

---
+++ transport/IsoTpAdapter.cpp
@@ -0,0 +1,65 @@
+#include "IsoTpAdapter.h"
+
+#include <iostream>
+
+
+// Constructeur
+IsoTpAdapter::IsoTpAdapter(
+    CanAdapter& canAdapter
+)
+:
+can(canAdapter)
+{
+
+}
+
+
+// Envoi d'un message ISO-TP
+bool IsoTpAdapter::sendMessage(
+    const std::vector<uint8_t>& data
+)
+{
+
+    std::cout 
+        << "[ISO-TP] Sending message..."
+        << std::endl;
+
+
+    /*
+        Pour cette première version :
+        on transmet directement les données
+        vers la couche CAN.
+
+        La segmentation ISO-TP réelle
+        sera ajoutée ensuite.
+    */
+
+    return can.send(data);
+
+}
+
+
+// Réception d'un message ISO-TP
+std::vector<uint8_t> IsoTpAdapter::receiveMessage()
+{
+
+    std::cout
+        << "[ISO-TP] Receiving message..."
+        << std::endl;
+
+
+    /*
+        Pour cette première version :
+        on récupère directement la trame
+        reçue par CAN.
+
+        Plus tard :
+        - assemblage des frames
+        - gestion First Frame
+        - gestion Consecutive Frame
+    */
+
+
+    return can.receive();
+
+}
